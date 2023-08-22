925574b579c273cdd4c4f0b6e5d4aae5d74a4361
gautierr <romain.gautier@sib.fr>
2023-08-22 15:09:35 +0200

    [FIX] update model tags

--- app/models/tags/Tags.py
+++ app/models/tags/Tags.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from sqlalchemy import Column, String, Integer, UniqueConstraint, ForeignKey, Boolean, Text
+from sqlalchemy import CheckConstraint, Column, String, Integer, UniqueConstraint, ForeignKey, Boolean, Text
 from sqlalchemy.orm import relationship, Mapped
 
 from app import db, ma
@@ -21,16 +21,20 @@ class Tags(Audit, db.Model):
 
 class TagAssociation(Audit, db.Model):
     __tablename__ = "tag_association"
+    __table_args__ = (
+        CheckConstraint( 'true' , name='line_fks_xor'), # XXX: voir les scripts de migration our la vraie definition de contrainte
+    )
 
     id = db.Column(Integer, primary_key=True)
     tag_id = Column(Integer, ForeignKey("tags.id"), nullable=False)
-    association_id = Column(Integer, nullable=False)
-    table_association_name = Column(String(255), nullable=False)
+
+    ademe = Column(Integer, ForeignKey("ademe.id"), nullable=True)
+    financial_ae = Column(Integer, ForeignKey("financial_ae.id"), nullable=True)
+
     # indique si le tag a été appliqué par un script auto ou non
     auto_applied = Column(Boolean, default=False, nullable=False)
 
     tag: Mapped[Tags] = relationship(back_populates="associations")
-    UniqueConstraint("tag_id", "association_id", "table_association_name", name="unique_association_tags")
 
 
 class TagsSchema(ma.SQLAlchemyAutoSchema):
