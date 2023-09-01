0083c022fac0d22852322df1688145a221b6baf0
Benjamin Bagot <benjamin.bagot@sib.fr>
2023-09-01 17:15:42 +0200

    [Fix] Format black sur les nouveaux fichiers

--- app/controller/ui_controller/UiController.py
+++ app/controller/ui_controller/UiController.py
@@ -13,22 +13,28 @@ api = Namespace(
 
 auth: OIDCAuthentication = current_app.extensions["auth"]
 
+
 @api.route("/annees")
 class GetYears(Resource):
     """
     Récupére la liste de toutes les années pour lesquelles on a des montants (engagés ou payés)
     :return:
     """
+
     @auth.token_auth("default", scopes_required=["openid"])
     @api.doc(security="Bearer")
     def get(self):
-        annees = db.session.execute(db.text('''
+        annees = db.session.execute(
+            db.text(
+                """
             SELECT ARRAY(
                 SELECT DISTINCT annee FROM public.financial_ae
                 UNION
                 SELECT DISTINCT annee FROM public.financial_cp
             )
-        ''')).scalar_one_or_none()
+        """
+            )
+        ).scalar_one_or_none()
         print(annees)
         if annees is None:
             return "", 204
--- app/controller/ui_controller/__init__.py
+++ app/controller/ui_controller/__init__.py
@@ -11,8 +11,11 @@ api_ui = Blueprint("api_ui", __name__)
 authorizations = {"Bearer": {"type": "apiKey", "in": "header", "name": "Authorization"}}
 
 api = Api(
-  api_ui,
-  prefix="/api/v1",
-  doc="/doc", description="API pour fournir des données à l'UI", authorizations=authorizations)
+    api_ui,
+    prefix="/api/v1",
+    doc="/doc",
+    description="API pour fournir des données à l'UI",
+    authorizations=authorizations,
+)
 
 api.add_namespace(uiApi)
