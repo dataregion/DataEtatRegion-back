20ecd1cda31b0780c8168c1b4a5a5835c04b30e7
Romain Gautier <romain.gautier@sib.fr>
2024-10-16 08:30:46 +0200

    [FIX] black

--- app/setup.py
+++ app/setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 
 self_path: str = (Path(__file__).parent).as_uri()
 
+
 def read_requirements(filename):
     with open(filename) as f:
         return f.read().splitlines()
+
 
 setup(
     install_requires=[
