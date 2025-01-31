fe5391a324ff1caef80fe7c4545aec377f2171f8
Romain Gautier <romain.gautier@sib.fr>
2025-01-31 14:42:57 +0100

    [FIX] initialization postgresql in tests

--- app/tests/DataEtatPostgresContainer.py
+++ app/tests/DataEtatPostgresContainer.py
@@ -4,14 +4,4 @@ from testcontainers.postgres import PostgresContainer
 class DataEtatPostgresContainer(PostgresContainer):
     def __init__(self):
         self.__image_name = "postgis/postgis:15-3.5"
-        return super(DataEtatPostgresContainer, self).__init__(image=self.__image_name)
-
-    def get_connection_url(self, host=None):
-        return super()._create_connection_url(
-            dialect="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            db_name=self.POSTGRES_DB,
-            host=host,
-            port=self.port_to_expose,
-        )
+        return super(DataEtatPostgresContainer, self).__init__(image=self.__image_name, driver="psycopg")
