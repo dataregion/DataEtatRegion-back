1a44a83bc96430fa04065558c5b22ce4a7e268e7
Benjamin Bagot <benjamin.bagot@sib.fr>
2025-03-11 11:42:05 +0100

    Add index to flatten_financial_lines

--- app/migrations/versions/20250310_vue_data_qpv_py/new/v_superset_data_qpv.sql
+++ app/migrations/versions/20250310_vue_data_qpv_py/new/v_superset_data_qpv.sql
@@ -6,4 +6,5 @@ CREATE VIEW superset_data_qpv AS
   SELECT ffl.*
     FROM flatten_financial_lines ffl
     INNER JOIN qpv_lieu_action qla ON qla.n_ej = ffl.n_ej;
-   
+
+CREATE INDEX idx_ffl_codes_qpv ON public.flatten_financial_lines USING btree (beneficiaire_qpv24_code, beneficiaire_qpv_code)
