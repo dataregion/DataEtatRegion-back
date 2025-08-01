009e58fd4798cefe0e3117da6e8412b49f0f4b03
Romain Gautier <romain.gautier@sib.fr>
2025-08-01 12:39:21 +0200

    [CHORE] update tests for demarches token

--- app/tests/services/demarches/test_tokens.py
+++ app/tests/services/demarches/test_tokens.py
@@ -1,67 +1,61 @@
-import json
-import os
-from pathlib import Path
-
+from app.services.demarches.tokens import TokenService
+from cryptography.fernet import Fernet
 import pytest
 
-from app.services.demarches.tokens import TokenService
-from models.entities.demarches.Token import Token
+from .fixtures import init_tokens, fernet_key
 
-_data = Path(os.path.dirname(__file__)) / "data"
+@pytest.fixture
+def fernet(fernet_key):
+    return Fernet(fernet_key)
 
+def decrypt_token(token: bytes, fernet: Fernet):
+    decrypted_token = token
+    decrypted_token = fernet.decrypt(decrypted_token) # type: ignore
+    decrypted_token = decrypted_token.decode()
+    return decrypted_token
 
-@pytest.fixture(scope="function")
-def init_tokens(database):
-    tokens = []
-    with open(_data / "tokens.json") as file:
-        for token in json.load(file):
-            token = Token(**token)
-            tokens.append(token)
-            database.session.add(token)
-
-    database.session.commit()
-    yield tokens
-
-    database.session.execute(database.delete(Token))
-    database.session.commit()
-
-
-def test_find_enabled_by_uuid_utilisateur(init_tokens):
+def test_find_enabled_by_uuid_utilisateur(fernet, init_tokens):
     tokens_user_1 = TokenService.find_enabled_by_uuid_utilisateur("USER1")
     assert len(tokens_user_1) == 1
-    assert tokens_user_1[0].token == "TOKEN1"
+
+    decrypted_token = decrypt_token(tokens_user_1[0]._token, fernet) # type: ignore
+    assert decrypted_token == "TOKEN1" # type: ignore
 
     TokenService.find_enabled_by_uuid_utilisateur("USER2")
     assert len(TokenService.find_enabled_by_uuid_utilisateur("USER2")) == 1
 
 
-def test_find_by_uuid_utilisateur_and_token_id(init_tokens):
+def test_find_by_uuid_utilisateur_and_token_id(init_tokens, fernet):
     token = TokenService.find_by_uuid_utilisateur_and_token_id("USER1", 101)
     assert token is not None
-    assert token.token == "TOKEN1"
+    decrypted_token = decrypt_token(token._token, fernet)
+    assert decrypted_token == "TOKEN1"
 
     token = TokenService.find_by_uuid_utilisateur_and_token_id("USER2", 102)
     assert token is not None
-    assert token.token == "TOKEN2"
+    decrypted_token = decrypt_token(token._token, fernet)
+    assert decrypted_token == "TOKEN2"
 
 
-def test_create(init_tokens):
+def test_create(init_tokens, fernet):
     uuid_utilisateur = "USER3"
 
     token_dto = TokenService.create("TOKEN3", "TOKEN3", uuid_utilisateur)
     token = TokenService.find_by_uuid_utilisateur_and_token_id(uuid_utilisateur, token_dto.id)
     assert token is not None
     assert token.enabled is True
-    assert token.token == "TOKEN3"
+    decrypted_token = decrypt_token(token._token, fernet) # type: ignore
+    assert decrypted_token == "TOKEN3"
 
 
-def test_create_existing(init_tokens):
+def test_create_existing(init_tokens, fernet):
     uuid_utilisateur = "USER3"
 
     token_dto = TokenService.create("TOKEN3", "TOKEN3", uuid_utilisateur)
     token = TokenService.find_by_uuid_utilisateur_and_token_id(uuid_utilisateur, token_dto.id)
     assert token is not None
-    assert token.token == "TOKEN3"
+    decrypted_token = decrypt_token(token._token, fernet)
+    assert decrypted_token == "TOKEN3"
 
     TokenService.delete(token.id, uuid_utilisateur)
     token = TokenService.find_by_uuid_utilisateur_and_token_id(uuid_utilisateur, token_dto.id)
@@ -72,7 +66,7 @@ def test_create_existing(init_tokens):
     assert token is not None and token.enabled is True
 
 
-def test_update(init_tokens):
+def test_update(init_tokens, fernet):
     uuid_utilisateur = "TOKEN_TO_UPDATE"
     nom_update = "TOKEN_NOM_UPDATE"
     token_update = "TOKEN_TOKEN_UPDATE"
@@ -82,7 +76,8 @@ def test_update(init_tokens):
     token = TokenService.find_by_uuid_utilisateur_and_token_id(uuid_utilisateur, token_dto.id)
 
     assert token is not None
-    assert token.nom == nom_update and token.token == token_update
+    decrypted_token = decrypt_token(token._token, fernet)
+    assert token.nom == nom_update and decrypted_token == token_update
 
 
 def test_delete(init_tokens):
--- models/src/models/entities/demarches/Token.py
+++ models/src/models/entities/demarches/Token.py
@@ -2,11 +2,8 @@ from dataclasses import dataclass
 
 from cryptography.fernet import Fernet
 
-# from flask import current_app
 from sqlalchemy import Boolean, Column, String, Integer, LargeBinary
 from models import _PersistenceBaseModelInstance
-
-# from app import db
 
 
 @dataclass
@@ -20,12 +17,23 @@ class Token(_PersistenceBaseModelInstance()):
 
     id: Column[int] = Column(Integer, primary_key=True, nullable=False)
     nom: Column[str] = Column(String, nullable=False)
-    _token: Column[LargeBinary] = Column("token", LargeBinary, nullable=False)
+    _token: Column[bytes] = Column("token", LargeBinary, nullable=False)
     uuid_utilisateur: Column[str] = Column(String, nullable=False, index=True)
     enabled: Column[bool] = Column(Boolean, nullable=False, server_default="TRUE")
 
     def get_token(self, fernet_key: str) -> str:
-        return Fernet(fernet_key).decrypt(self._token).decode("utf-8")
+        return Fernet(fernet_key).decrypt(self._token).decode("utf-8")  # type: ignore
 
-    def set_token(self, value: str, fernet_key: str):
-        self._token = Fernet(fernet_key).encrypt(value.encode("utf-8"))
+    def set_token(self, fernet_key: str, value: str):
+        self._token = Fernet(fernet_key).encrypt(value.encode("utf-8"))  # type: ignore
+
+    @staticmethod
+    def make_from_kwargs(fernet_key: str, **kwargs):
+
+        _token = kwargs.pop("token", None)
+        _fernet_key = kwargs.pop("fernet_key", fernet_key)
+
+        token = Token(**kwargs)
+        token.set_token(fernet_key=_fernet_key, value=_token)
+
+        return token
