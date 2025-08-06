c6f578192c15e3f6986a9def9991c727d3da5dfc
Benjamin Bagot <benjamin.bagot@sib.fr>
2025-08-06 11:21:13 +0200

    Fix schema for success in swagger

--- apis/src/apis/apps/referentiels/routers/healthcheck.py
+++ apis/src/apis/apps/referentiels/routers/healthcheck.py
@@ -1,15 +1,22 @@
 from http import HTTPStatus
 
+from apis.shared.openapi_config import build_api_success_response
 from fastapi import APIRouter
 
 from apis.shared.decorators import handle_exceptions
 from apis.shared.models import APISuccess
+from fastapi.responses import JSONResponse
 
 
 router = APIRouter()
 
 
-@router.get("")
+@router.get(
+    "",
+    summary="Find all QPV by annee",
+    response_class=JSONResponse,
+    responses=build_api_success_response(),
+)
 @handle_exceptions
 def healthcheck():
     return APISuccess(status_code=HTTPStatus.OK, message="Healthcheck OK", data=None)
