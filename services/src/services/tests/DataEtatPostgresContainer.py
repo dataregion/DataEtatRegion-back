4b12b9580f669dde449c30adfdfb008abc21fea1
Romain Gautier <romain.gautier@sib.fr>
2025-08-07 16:51:23 +0200

    [CHORE] black

--- services/src/services/tests/DataEtatPostgresContainer.py
+++ services/src/services/tests/DataEtatPostgresContainer.py
@@ -4,4 +4,6 @@ from testcontainers.postgres import PostgresContainer
 class DataEtatPostgresContainer(PostgresContainer):
     def __init__(self):
         self.__image_name = "postgis/postgis:15-3.5"
-        return super(DataEtatPostgresContainer, self).__init__(image=self.__image_name, driver="psycopg")
+        return super(DataEtatPostgresContainer, self).__init__(
+            image=self.__image_name, driver="psycopg"
+        )
