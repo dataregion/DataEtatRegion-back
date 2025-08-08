1232be1eed3db8b998476e2eeaf372bb06147043
Benjamin Bagot <benjamin.bagot@sib.fr>
2025-08-08 11:21:28 +0200

    Black 3

--- models/src/models/entities/common/SyncedWithGrist.py
+++ models/src/models/entities/common/SyncedWithGrist.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class _SyncedWithGrist(_PersistenceBaseModelInstance()):
     __abstract__ = True
-    synchro_grist_id: Mapped[int] = mapped_column(ForeignKey("synchro_grist.id"), nullable=True)
+    synchro_grist_id: Mapped[int] = mapped_column(
+        ForeignKey("synchro_grist.id"), nullable=True
+    )
     grist_row_id: Mapped[int] = mapped_column(Integer, unique=True)
-    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, server_default="FALSE")
+    is_deleted: Mapped[bool] = mapped_column(
+        Boolean, default=False, server_default="FALSE"
+    )
--- models/src/models/entities/refs/CodeProgramme.py
+++ models/src/models/entities/refs/CodeProgramme.py
@@ -15,13 +15,17 @@ class CodeProgramme(_Audit, _SyncedWithGrist, _PersistenceBaseModelInstance()):
     code_ministere: Column[str] = Column(
         String, ForeignKey("ref_ministere.code"), nullable=True
     )
-    code_theme: Column[str] = Column(String, ForeignKey("ref_theme.code"), nullable=True)
+    code_theme: Column[str] = Column(
+        String, ForeignKey("ref_theme.code"), nullable=True
+    )
     theme: Column[int] = Column(Integer, ForeignKey("ref_theme.id"), nullable=True)
 
     label: Column[str] = Column(String)
     description: Column[str] = Column(Text)
 
-    theme_r: Mapped[Theme] = relationship("Theme", uselist=False, lazy="select", foreign_keys=code_theme)
+    theme_r: Mapped[Theme] = relationship(
+        "Theme", uselist=False, lazy="select", foreign_keys=code_theme
+    )
     # permet de remonter uniquement le label
     label_theme = association_proxy("theme_r", "label")
 
