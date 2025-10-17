a0da9025728bb7feb946496216b8527a101f6e35
Romain Gautier <romain.gautier@sib.fr>
2025-10-17 12:19:24 +0200

    [FEAT] Ajoute l'utilitaire pour avoir les métriques de cache functools

--- services/src/services/utilities/observability.py
+++ services/src/services/utilities/observability.py
@@ -2,10 +2,17 @@
 Module pour l'observabilité. ie. prometheus
 """
 
+import logging
 from contextlib import contextmanager
 from functools import wraps
 from prometheus_client import Gauge, Summary
 import time
+from functools import _CacheInfo
+
+_logger = logging.getLogger(__name__)
+
+_SUMMARY_CACHE = {}
+__GAUGE_CACHE = {}
 
 
 def get_full_func_name(func):
@@ -20,9 +27,6 @@ def sanitize_prom_metric_name(name):
     return transformed
 
 
-_SUMMARY_CACHE = {}
-
-
 def _get_summary_of_time_or_default(name) -> Summary:
     if name not in _SUMMARY_CACHE:
         summary = Summary(f"{name}_latency_seconds", f"Performance for {name}")
@@ -30,12 +34,17 @@ def _get_summary_of_time_or_default(name) -> Summary:
     return _SUMMARY_CACHE[name]
 
 
-__GAUGE_CACHE = {}
-
-
 def _get_gauge_of_currently_executing_or_default(name) -> Gauge:
     if name not in __GAUGE_CACHE:
         gauge = Gauge(f"{name}_inprogress", f"Currently executing {name}")
+        __GAUGE_CACHE[name] = gauge
+    return __GAUGE_CACHE[name]
+
+
+def _get_gauge_of_cache_or_default(name: str, stat_type: str) -> Gauge:
+    name = f"{name}_cache_{stat_type}"
+    if name not in __GAUGE_CACHE:
+        gauge = Gauge(f"{name}", f"{stat_type} gauge of cache for {name}")
         __GAUGE_CACHE[name] = gauge
     return __GAUGE_CACHE[name]
 
@@ -107,6 +116,43 @@ def gauge_of_currently_executing():
         @gauge.track_inprogress()
         def inner_wrapper(*args, **kwargs):
             return func(*args, **kwargs)
+
+        return inner_wrapper
+
+    return wrapper
+
+
+def cache_stats():
+    """Expose des métriques prometheus concernant les statistiques de cache (hit/miss) de la fonction"""
+
+    def wrapper(func):
+        name = get_full_func_name(func)
+        name = sanitize_prom_metric_name(name)
+
+        hit_gauge = _get_gauge_of_cache_or_default(name, "hit")
+        currsize_gauge = _get_gauge_of_cache_or_default(name, "currsize")
+        miss_gauge = _get_gauge_of_cache_or_default(name, "miss")
+
+        @wraps(func)
+        def inner_wrapper(*args, **kwargs):
+            try:
+                results = func(*args, **kwargs)
+                return results
+            finally:
+                if not hasattr(func, "cache_info"):
+                    _logger.warning(
+                        "La fonction décorée ne possède pas d'informations de cache. Assurez-vous qu'elle utilise un mécanisme de cache."
+                    )
+                else:
+                    try:
+                        cache_info: _CacheInfo = func.cache_info()
+                        hit_gauge.set(cache_info.hits)
+                        miss_gauge.set(cache_info.misses)
+                        currsize_gauge.set(cache_info.currsize)
+                    except Exception as e:
+                        _logger.error(
+                            f"Erreur lors de la récupération des informations de cache pour la fonction {name}: {e}"
+                        )
 
         return inner_wrapper
 
