d085c10e653e9c40254574224d51bcbcecbe7f32
Benjamin Bagot <benjamin.bagot@sib.fr>
2025-12-02 09:53:16 +0100

    Fix tests conflict

--- apis/tests/apps/qpv/test_colonnes_service.py
+++ apis/tests/apps/qpv/test_colonnes_service.py
@@ -2,20 +2,11 @@ from tests import _assert_can_jsonize
 
 from services.qpv.colonnes import get_list_colonnes_tableau
 
-from fastapi.testclient import TestClient
-
 
 ####
 #
 def test_get_list_colonnes_tableau_is_serializable():
     colonnes = get_list_colonnes_tableau()
-    assert colonnes is not None
-    dumped = _assert_can_jsonize(colonnes, mode="json")
-    assert dumped is not None
-
-
-def test_get_list_colonnes_grouping_is_serializable():
-    colonnes = get_list_colonnes_grouping()
     assert colonnes is not None
     dumped = _assert_can_jsonize(colonnes, mode="json")
     assert dumped is not None
