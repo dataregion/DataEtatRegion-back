fe7cc56d60bf8315716fba15bb8e52c0e81d9fdc
Romain Gautier <romain.gautier@sib.fr>
2026-01-06 14:09:43 +0100

    [CHORE] raise the threshold of load tests

--- tests_e2e/src/tests_e2e/load_tests/thresholds.py
+++ tests_e2e/src/tests_e2e/load_tests/thresholds.py
@@ -1,4 +1,4 @@
 GLOBAL_PERFORMANCE_THRESHOLDS = {
-    "GET /lignes": {"avg": 1500, "max": 2000},
+    "GET /lignes": {"avg": 2000, "max": 3000},
     "GET /lignes?beneficiaires": {"avg": 500, "max": 1000},
 }
