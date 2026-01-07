09e52b16fe9c7f79b0b710fe6ae007d381134a7e
Romain Gautier <romain.gautier@sib.fr>
2026-01-07 11:10:38 +0100

    [FIX] fix la cli pour data subvention

--- apis/src/apis/cli/api_externes.py
+++ apis/src/apis/cli/api_externes.py
@@ -1,3 +1,4 @@
+from dataclasses import asdict, is_dataclass
 import json
 import typer
 
@@ -12,6 +13,7 @@ api_subventions_client_typer = typer.Typer(help="Client data subventions")
 )
 def get_subvention_pour_etablissement(siret: str):
     subventions = data_subventions_client.get_subventions_pour_etablissement(siret)
+    subventions = [asdict(s) if is_dataclass(s) else s for s in subventions]
     output = json.dumps(subventions)
     typer.echo(output)
 
@@ -19,5 +21,6 @@ def get_subvention_pour_etablissement(siret: str):
 @api_subventions_client_typer.command("representants_legaux", help="Récupère les représentants légaux")
 def representant_legaux_pour_etablissement(siret: str):
     representant_legaux = data_subventions_client.get_representants_legaux_pour_etablissement(siret)
+    representant_legaux = [asdict(r) if is_dataclass(r) else r for r in representant_legaux]
     output = json.dumps(representant_legaux)
     typer.echo(output)
