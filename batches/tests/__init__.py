9efd94ae662fa2de1de6cfea96bf0def4e442dee
Benjamin Bagot <benjamin.bagot@sib.fr>
2026-01-30 17:13:44 +0100

    Fix localhost

--- batches/tests/__init__.py
+++ batches/tests/__init__.py
@@ -5,4 +5,4 @@ from pathlib import Path
 TESTS_PATH = Path(os.path.dirname(__file__))
 
 # On set ici l'URL du container Prefect, sinon Prefect va s'init avec le port 4200
-os.environ["PREFECT_API_URL"] = "http://127.0.0.1:4281/api"
+os.environ["PREFECT_API_URL"] = "http://localhost:4281/api"
