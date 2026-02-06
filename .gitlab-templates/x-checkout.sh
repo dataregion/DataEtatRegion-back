a5a56715212bc186e0583285f0e8f92cbacbc7f9
Romain Gautier <romain.gautier@sib.fr>
2026-02-07 00:04:18 +0100

    [FEAT] support du refresh de dependance via suppression du requirements.external.txt

--- .gitlab-templates/x-checkout.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -x
-
-git fetch
-git checkout "$CI_COMMIT_BRANCH"
-git reset --hard "origin/$CI_COMMIT_BRANCH"
-
-set +x
--- .gitlab-templates/x-commit.sh
+++ .gitlab-templates/x-commit.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 PAT="${1:?PAT token required as second argument}"
 COMMIT_MESSAGE="${2:-[FORGE] Mise à jour}"
 
+branch="${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME:-${CI_COMMIT_BRANCH}}"
+
 # Configure git to use the PAT for authentication
 git config --local credential.helper '!f() { echo "username=oauth2"; echo "password='$PAT'"; }; f'
 git remote set-url origin "https://oauth2:$PAT@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
@@ -12,4 +14,5 @@ git remote set-url origin "https://oauth2:$PAT@${CI_SERVER_HOST}/${CI_PROJECT_PA
 echo >&2 "Will commit the local changes!"
 git add -u
 git commit -m "$COMMIT_MESSAGE"
-git push origin "$CI_COMMIT_BRANCH"
+git checkout -b "$branch"
+git push origin "$branch"
