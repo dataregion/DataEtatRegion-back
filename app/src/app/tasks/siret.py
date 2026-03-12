6d92821ed622978fd4c352f63e5e0bd68da13469
Romain Gautier <romain.gautier@sib.fr>
2026-03-12 15:01:57 +0100

    [CHORE] enlève le code d'update_all_siret

--- app/src/app/tasks/siret.py
+++ app/src/app/tasks/siret.py
@@ -13,73 +13,8 @@ from flask import current_app
 from app import celeryapp, db
 from models.entities.refs.Siret import Siret
 
-from app.services.siret import AppUpdateRefSiretService
-from app.clients.entreprise import LimitHitError
-
-from app.utilities.bucketting import which_bucket
-
-__all__ = (
-    "update_one_fifth_of_sirets",
-    "update_all_siret_task",
-)
-
 logger = logging.getLogger()
 celery = celeryapp.celery
-
-
-@celery.task(name="update_one_fifth_of_sirets", bind=True)
-def update_one_fifth_of_sirets(self, which_fifth: int):
-    buckets_size = 5
-    if which_fifth < 0 or which_fifth > buckets_size:
-        raise ValueError(f"which_fifth is {which_fifth}. It should be [0, {buckets_size}]")
-
-    stmt = db.select(Siret.code).order_by(Siret.id)
-    codes = db.session.execute(stmt).scalars()
-
-    total = 0
-    for i, code in enumerate(codes):
-        code = int(code)
-        bucket_number = which_bucket(code, buckets_size)
-        if bucket_number != which_fifth:
-            continue
-
-        total += 1
-        subtask("update_siret_task").delay(i, code)
-
-    return f"Commande d'update envoyée pour {total} sirets"
-
-
-@celery.task(name="update_all_siret_task", bind=True)
-def update_all_siret_task(self):
-    stmt = db.select(Siret.code).order_by(Siret.id)
-    codes = db.session.execute(stmt).scalars()
-
-    for i, code in enumerate(codes):
-        subtask("update_siret_task").delay(i, code)
-
-
-@celery.task(name="update_siret_task", bind=True)
-def update_siret_task(self, index: int, code: str):
-    try:
-        siret = AppUpdateRefSiretService.create_for_app().update_siret_from_api_entreprise(db.session, code)
-    except LimitHitError as e:
-        delay = (e.delay) + 5
-        logger.info(
-            f"[UPDATE][SIRET][{code}] Limite d'appel à l'API atteinte Ré essai de la tâche dans {str(delay)} secondes"
-        )
-        # XXX: max_retries=None ne désactive pas le mécanisme
-        # de retry max contrairement à ce que stipule la doc !
-        # on met donc un grand nombre.
-        self.retry(countdown=delay, max_retries=1000, retry_jitter=True)
-        return
-
-    try:
-        db.session.add(siret)
-        db.session.commit()
-    except Exception as _:
-        logger.error(f"[UPDATE][SIRET][{code}] Erreur lors de la mise à jour en base de données. Rollback.")
-        db.session.rollback()
-        raise
 
 
 @celery.task(name="update_link_siret_qpv_from_website", bind=True)
