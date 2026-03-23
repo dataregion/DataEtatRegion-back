dcb67c7be9366eb1542cc0ed681e6821f25582c5
Benjamin Bagot <benjamin.bagot@sib.fr>
2026-03-23 14:15:59 +0100

    Fix ref_action nullable

--- batches/src/batches/share/validation/import_qpv_lieu_action_schema.py
+++ batches/src/batches/share/validation/import_qpv_lieu_action_schema.py
@@ -5,7 +5,7 @@ from batches.share.validation.validation_schema_csv import ExpectedColonnes, Val
 class ImportQpvLieuActionSchema(ValidationSchemaCsv):
     colonnes: ExpectedColonnes = [
         GenericColonneCsv(name="annee_exercice", dtype=int, required=True, example=2025),
-        GenericColonneCsv(name="ref_action", dtype=str, required=True, example="P147-001"),
+        GenericColonneCsv(name="ref_action", dtype=str, required=False, example="P147-001"),
         GenericColonneCsv(name="ej", dtype=str, required=True, example="1234567890"),
         GenericColonneCsv(name="code_qpv", dtype=str, required=True, example="QP044004"),
         GenericColonneCsv(name="code_qpv24", dtype=str, required=True, example="QN04404M"),
